3d80e1662fd0171bf3e43c67a2f513ccfe9deced
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2018-08-30 15:26:40 -0700

    typos

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 
 setup(
-    name='lume-genesis'
-    version = '0.1'
-    packages = ['genesis']
+    name='lume-genesis',
+    version = '0.1',
+    packages = ['genesis'],
     long_description=open('README.md').read()
 )
