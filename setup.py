6d3833c28e7367f853b1ecb8bd5fd545bd836aa3
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2020-01-15 10:03:37 -0800

    Preparing for first release

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path, environ
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split()
 
 
 
 setup(
     name='lume-genesis',
-    version = '0.1',
-    packages = ['genesis'],
-    long_description=open('README.md').read()
+    version = 'v0.1.0',
+    packages=find_packages(),  
+    package_dir={'genesis':'genesis'},
+    url='https://github.com/slaclab/lume-genesis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.6'
 )
