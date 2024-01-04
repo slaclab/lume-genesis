97d995468efeac34a2f948909fb446fcc1dc8f61
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2024-01-04 07:13:40 -0800

    Try skipping FODO notebooks

--- scripts/execute_notebooks.bash
+++ scripts/execute_notebooks.bash
@@ -3,7 +3,7 @@ set -e
 
 NOTEBOOKS=$(find docs/examples/genesis4/ -type f -name "*.ipynb" -not -path '*/.*')
 
-SKIP="perlmutter" 
+SKIP_PATTERNS=("perlmutter" "fodo")  
 
 #echo $NOTEBOOKS
 
@@ -12,11 +12,19 @@ export PYDEVD_DISABLE_FILE_VALIDATION=1
 
 for file in $NOTEBOOKS
 do
-    if [[ "$file" == *"$SKIP"* ]]; then 
-        echo "Skipping $file"
-        continue
+    should_skip=false
+    for SKIP in "${SKIP_PATTERNS[@]}"; do
+        if [[ "$file" == *"$SKIP"* ]]; then
+           should_skip=true
+           break
+       fi
+    done
+
+    if [ "$should_skip" = true ]; then
+        echo "Skipping file $file"
+    # Add your skip logic here
+    else
+        echo "Processing file $file"
+        jupyter nbconvert --to notebook --execute $file --inplace
     fi
-  
-    echo "Executing $file"
-    jupyter nbconvert --to notebook --execute $file --inplace
 done
