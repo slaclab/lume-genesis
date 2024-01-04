b88a8d3ecde94d5b9cc23585f5edd73ee89ef32c
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2024-01-03 16:05:45 -0800

    Build system overhaul
    This follows the changes from https://github.com/ChristopherMayes/lume-impact/pull/21

--- genesis/__init__.py
+++ genesis/__init__.py
@@ -1,14 +1,17 @@
-import sys
 from warnings import warn
 from .genesis2 import Genesis2
 from .version4.genesis4 import Genesis4
-from . import _version
 
-assert sys.version_info >= (3, 7), "Python 3.7 or greater required."
-__version__ = _version.get_versions()["version"]
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
 
+__all__ = [
+    'Genesis2',
+    'Genesis4',
+]
 
-# from .genesis import Genesis
 def Genesis(*args, **kwargs):
     warn("The Genesis class has been renamed to Genesis2.")
     return Genesis2(*args, **kwargs)
