90cb59f143094809d56109f69be95d06baa08a83
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-05-22 09:44:47 -0700

    FIX: add example3 to the skip list as it takes forever

--- scripts/execute_notebooks.bash
+++ scripts/execute_notebooks.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-cd docs/examples/genesis4 || exit 1
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd "$REPO_ROOT/docs/examples/genesis4" || exit 1
 
 NOTEBOOKS=$(git ls-files "*.ipynb")
 
-SKIP_PATTERNS=("perlmutter" "fodo")
+SKIP_PATTERNS=("perlmutter" "fodo" "Example3")
 
 # Silence Jupyterlab warning
 export PYDEVD_DISABLE_FILE_VALIDATION=1
@@ -21,11 +23,12 @@ do
     done
 
     if [ "$should_skip" = true ]; then
-        echo "Skipping: $file"
+        echo "* Skipping: $(basename "$file") as it matches a skip_pattern"
     else
-        echo "Processing: $file"
-        pushd "$(dirname "$file")" || exit
+        pushd "$(dirname "$file")" > /dev/null || exit
+        echo "* Processing: $(basename "$file") in $PWD"
         jupyter nbconvert --to notebook --execute "$(basename "$file")" --inplace
-        popd || exit
+        popd > /dev/null || exit
     fi
+    echo
 done
